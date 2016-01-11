847b6a693ba7c74208fbf2ec527c06b5a85c9eee
sageknives <sageknives@gmail.com>
2016-01-11 10:23:40 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+  console.log(" in index");
+});
+app.get('/fun', function(request, response) {
+  response.render('pages/index');
+  console.log(" in fun");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
